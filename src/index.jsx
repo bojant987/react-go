5bf974e131414d788f6ebd039689825a5173fe5b
bojant987 <bojant987@hotmail.com>
2018-09-06 19:13:20 +0200

    Fixed render of app in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -6,15 +6,17 @@ import App from './App';
 
 require('../assets/styles/main.scss');
 
-const render = Component => {
-	ReactDOM.render(
-		<HotContainer>
-			<Component />
-		</HotContainer>,
-		document.getElementById('root')
-	);
-};
+if (process.env.NODE_ENV === 'development') {
+	const render = Component => {
+		ReactDOM.render(
+			<HotContainer>
+				<Component />
+			</HotContainer>,
+			document.getElementById('root')
+		);
+	};
 
-render(App);
-
-ReactDOM.render(<App />, document.getElementById('root'));
+	render(App);
+} else {
+	ReactDOM.render(<App />, document.getElementById('root'));
+}
